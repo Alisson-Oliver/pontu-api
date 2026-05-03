41292b8e8c65d4b72c796e8dcb2ed1625c48b83c
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-05-03 19:34:17 -0300

    feat(security): implement SecurityFilter for JWT token validation and authentication

--- src/main/java/br/com/pontu/api/config/SecurityConfig.java
+++ src/main/java/br/com/pontu/api/config/SecurityConfig.java
@@ -1,24 +1,34 @@
 package br.com.pontu.api.config;
 
+import br.com.pontu.api.filters.SecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final SecurityFilter securityFilter;
+
+    SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
             )
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .oauth2Login((oauth2 -> oauth2
                 .defaultSuccessUrl("/auth/success", true)
             ));
---
+++ src/main/java/br/com/pontu/api/filters/SecurityFilter.java
@@ -0,0 +1,54 @@
+package br.com.pontu.api.filters;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.pontu.api.entities.User;
+import br.com.pontu.api.repositories.UserRepository;
+import br.com.pontu.api.services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, 
+        HttpServletResponse response, 
+        FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        var token = recoverToken(request);
+        if (token != null) {
+            var login = jwtService.validateToken(token);
+            if (login != null) {
+                User user = userRepository.findByEmail(login).orElseThrow();
+                
+                var authentication = new UsernamePasswordAuthenticationToken(
+                    user, null, null
+                );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
