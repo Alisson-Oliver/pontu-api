7124c4c5a155e96e74119ab2049b57942d568dcb
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-05-03 17:53:35 -0300

    feat(security): implement SecurityConfig for HTTP security configuration

---
+++ src/main/java/br/com/pontu/api/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package br.com.pontu.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2Login((oauth2 -> oauth2
+                .defaultSuccessUrl("/auth/success", true)
+            ));
+
+            return http.build();
+    }
+}
