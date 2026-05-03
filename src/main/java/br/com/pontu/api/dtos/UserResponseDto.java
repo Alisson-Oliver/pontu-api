befa4fcaf818d04a0efe00dc70f3eb93c5fe4a30
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-05-03 19:34:05 -0300

    feat(dto): create UserResponseDto for user data transfer

---
+++ src/main/java/br/com/pontu/api/dtos/UserResponseDto.java
@@ -0,0 +1,25 @@
+package br.com.pontu.api.dtos;
+
+import br.com.pontu.api.entities.User;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public record UserResponseDto(
+        Long id,
+        String name,
+        String email,
+        String photoUrl,
+        LocalDateTime createdAt
+) {
+
+    public static UserResponseDto fromEntity(User user) {
+        return  new UserResponseDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getPhotoUrl(),
+                user.getUpdateAt()
+        );
+    }
+}
