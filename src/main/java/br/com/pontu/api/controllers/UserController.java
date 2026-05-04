ad1cd13c42c03d285fea6c1c0f90ba41844b49e7
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-05-04 00:51:36 -0300

    feat(config): create ConfigController and ConfigRepository for configuration management

--- src/main/java/br/com/pontu/api/controllers/UserController.java
+++ src/main/java/br/com/pontu/api/controllers/UserController.java
@@ -1,7 +1,6 @@
 package br.com.pontu.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
