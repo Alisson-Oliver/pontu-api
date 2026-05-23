20623d8d887308e7aafd6d0fe8337148af2c02ea
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-05-23 11:54:42 -0300

    feat(timeEntry): enhance TimeEntry management with new endpoints and validation

--- src/main/java/br/com/pontu/api/dtos/records/TimeEntryResponseDto.java
+++ src/main/java/br/com/pontu/api/dtos/records/TimeEntryResponseDto.java
@@ -1,6 +1,7 @@
 package br.com.pontu.api.dtos.records;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import br.com.pontu.api.entities.TimeEntry;
 import br.com.pontu.api.enums.TimeEntryType;
@@ -20,5 +21,13 @@ public record TimeEntryResponseDto(
                timeEntry.getType(),
                timeEntry.getObservation() 
         );
+    }
+
+    public static List<TimeEntryResponseDto> transformList(List<TimeEntry> timeEntries) {
+        return timeEntries.stream()
+            .map(e -> new TimeEntryResponseDto(
+                e.getId(), e.getTimestamp(), e.getType(), e.getObservation()
+            )
+        ).toList();
     }
 }
