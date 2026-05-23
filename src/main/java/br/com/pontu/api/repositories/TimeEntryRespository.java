7d29ea0b8523beacd490c7925d3a508861d93b4a
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-05-23 11:56:06 -0300

    feat(timeEntry): add repository methods for TimeEntry management

--- src/main/java/br/com/pontu/api/repositories/TimeEntryRespository.java
+++ src/main/java/br/com/pontu/api/repositories/TimeEntryRespository.java
@@ -1,8 +1,18 @@
 package br.com.pontu.api.repositories;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.pontu.api.entities.TimeEntry;
+import br.com.pontu.api.enums.TimeEntryType;
+
+import java.util.List;
+
 
 public interface TimeEntryRespository extends JpaRepository<TimeEntry, Long> {
+    boolean existsByTypeAndCompetenceDate(TimeEntryType type, LocalDate competenceDate, Long userId);
+    List<TimeEntry> findByCompetenceDateAndUserId(LocalDate competenceDate, Long userId);
+    List<TimeEntry> findByUserId(Long id);
+    long deleteByIdAndUserId(Long id, Long userId);
 }
