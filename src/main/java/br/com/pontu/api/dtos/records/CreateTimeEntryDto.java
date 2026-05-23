78f66f5eb400d8ac93aaf9b2d611b9bbd3babdd0
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-05-23 11:55:29 -0300

    feat(timeEntry): refactor CreateTimeEntryDto to remove timestamp and simplify entity conversion

--- src/main/java/br/com/pontu/api/dtos/records/CreateTimeEntryDto.java
+++ src/main/java/br/com/pontu/api/dtos/records/CreateTimeEntryDto.java
@@ -1,25 +1,26 @@
 package br.com.pontu.api.dtos.records;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import br.com.pontu.api.enums.TimeEntryType;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import br.com.pontu.api.entities.TimeEntry;
 
-
 public record CreateTimeEntryDto(
     @NotNull()
-    LocalDateTime timestamp,
-
-    @NotBlank()
-    TimeEntryType type,
+    TimeEntryType type, 
 
     @Size(max = 500)
     String observation
 ) {
     public TimeEntry toEntity() {
-        return new TimeEntry(null, this.timestamp, this.type, this.observation, null);
+        return new TimeEntry(
+            LocalDateTime.now(), 
+            LocalDate.now(),
+            this.type, 
+            this.observation
+        );
     }
 }
