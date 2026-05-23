c606297087baa150625fc4978e249a3e97cc2407
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-05-23 11:56:25 -0300

    feat(timeEntry): update TimeEntryController to use consistent endpoint naming and improve method organization

--- src/main/java/br/com/pontu/api/controllers/TimeEntryController.java
+++ src/main/java/br/com/pontu/api/controllers/TimeEntryController.java
@@ -9,14 +9,18 @@ import br.com.pontu.api.entities.User;
 import br.com.pontu.api.services.TimeEntryService;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("timeEntry")
+@RequestMapping("time-entries")
 public class TimeEntryController {
 
     @Autowired
@@ -24,10 +28,25 @@ public class TimeEntryController {
     
     @PostMapping()
     public TimeEntryResponseDto create(
-        @RequestBody @Valid CreateTimeEntryDto data,
-        @AuthenticationPrincipal User user
+        @RequestBody @Valid CreateTimeEntryDto data, @AuthenticationPrincipal User user
     ) {
         return service.create(data, user);
     }
-    
+
+    @GetMapping("me/today")
+    public List<TimeEntryResponseDto> getToday(@AuthenticationPrincipal User user) {
+        return service.getTimeEntriesToday(user.getId());
+    }
+
+    @GetMapping("me")
+    public List<TimeEntryResponseDto> getAllByUser(@AuthenticationPrincipal User user) {
+        return service.getAllByUser(user.getId());
+    }
+
+    @DeleteMapping("me/{id}")
+    public void delete(
+        @AuthenticationPrincipal User user, @PathVariable("id") Long id
+    ) {
+        service.deleteById(id, user.getId());
+    }
 }
